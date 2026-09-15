5823f1a5c10ed85e0fac0e71309a2765ee3fde15
hzshop0 <hasanfneish82@gmail.com>
2026-09-15 16:10:14 +0300

    Update history.js

--- js/navigation/history.js
+++ js/navigation/history.js
@@ -1,1 +1,19 @@
+const NAVIGATION_HISTORY = {
+  back() {
+    if (window.history.length > 1) {
+      window.history.back();
+    } else {
+      NAVIGATION.home();
+    }
+  },
 
+  forward() {
+    window.history.forward();
+  },
+
+  replace(path) {
+    if (!path) return;
+
+    window.location.replace(path);
+  }
+};
