15c5041b4a2bffadf4189a7ec7cabdd4f097c9e7
hzshop0 <hasanfneish82@gmail.com>
2026-09-15 15:56:42 +0300

    Update store.js

--- config/store.js
+++ config/store.js
@@ -1,1 +1,12 @@
-
+const STORE_CONFIG = {
+  name: "HZ.shop",
+  currency: "$",
+  languages: ["ar", "en"],
+  defaultLanguage: "ar",
+  direction: {
+    ar: "rtl",
+    en: "ltr"
+  },
+  deliveryFee: 4,
+  brandName: "HZ.shop"
+};
