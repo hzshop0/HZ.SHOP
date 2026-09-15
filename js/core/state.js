07f423d18390dc05306e3a6d1d996159241a247e
hzshop0 <hasanfneish82@gmail.com>
2026-09-15 16:00:12 +0300

    Update state.js

--- js/core/state.js
+++ js/core/state.js
@@ -1,1 +1,25 @@
+const APP_STATE = {
+  language: "ar",
+  direction: "rtl",
 
+  customer: null,
+
+  cart: {
+    items: [],
+    count: 0,
+    subtotal: 0,
+    deliveryFee: 4,
+    total: 4
+  },
+
+  wishlist: [],
+
+  recentlyViewed: [],
+
+  notifications: [],
+
+  search: {
+    query: "",
+    results: []
+  }
+};
