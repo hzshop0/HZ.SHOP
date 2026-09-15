023a18aa6cf015812ee1de9cbf81b66bfd6fd88b
hzshop0 <hasanfneish82@gmail.com>
2026-09-15 16:04:55 +0300

    Update utils.js

--- js/core/utils.js
+++ js/core/utils.js
@@ -1,1 +1,46 @@
+const UTILS = {
+  escapeHTML(value = "") {
+    return String(value)
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+  },
 
+  formatPrice(value) {
+    const number = Number(value);
+
+    if (!Number.isFinite(number)) {
+      return "$0";
+    }
+
+    return `$${number.toFixed(2)}`;
+  },
+
+  formatNumber(value) {
+    const number = Number(value);
+
+    if (!Number.isFinite(number)) {
+      return "0";
+    }
+
+    return number.toLocaleString();
+  },
+
+  isRTL(language = "ar") {
+    return language === "ar";
+  },
+
+  debounce(callback, delay = 300) {
+    let timer;
+
+    return (...args) => {
+      clearTimeout(timer);
+
+      timer = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    };
+  }
+};
