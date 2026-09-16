2d2cce27708cf6f55009252ce634a0ef0dae5e23
hzshop0 <hasanfneish82@gmail.com>
2026-09-16 18:27:54 +0300

    Refactor banner.js for improved readability

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,50 +1,87 @@
 const BANNER = {
+
   create(banner = {}) {
-    const template = document.querySelector(
-      "#bannerTemplate"
-    );
+
+    const template =
+      document.querySelector(
+        "#bannerTemplate"
+      );
 
     if (!template) {
       return null;
     }
 
-    const element = template.content
-      .firstElementChild
-      .cloneNode(true);
 
-    this.update(element, banner);
+    const element =
+      template.content
+        .firstElementChild
+        .cloneNode(true);
+
+
+    this.update(
+      element,
+      banner
+    );
+
 
     return element;
   },
 
-  update(element, banner = {}) {
+
+  update(
+    element,
+    banner = {}
+  ) {
+
     if (!element) return;
+
 
     const id =
       banner.id ??
       banner.bannerId ??
       "";
 
-    element.dataset.bannerId = id;
 
-    const link = element.querySelector(
-      ".store-banner-link"
-    );
+    element.dataset.bannerId =
+      id;
 
-    const image = element.querySelector(
-      ".store-banner-image"
-    );
+
+    const link =
+      element.querySelector(
+        ".store-banner-link"
+      );
+
+
+    const image =
+      element.querySelector(
+        ".store-banner-image"
+      );
+
 
     if (link) {
-      link.href = banner.link || "#";
+
+      link.href =
+        banner.link || "#";
+
     }
 
+
     if (image) {
-      image.src = banner.image || "";
+
+      image.src =
+        banner.image || "";
+
       image.alt =
         banner.alt ||
         banner.title ||
         "HZ.shop";
+
     }
+
   }
+
 };
+
+
+window.BANNER =
+  BANNER;
